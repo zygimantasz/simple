fdf3ea9583ba20cfef66896c76e583267ea2c1ba
“zygimantaszgit config --global user.email “zygimantas.zibutis@ksdigital.lt <zygimantas.zibutis@ksdigital.lt>
2017-02-22 16:49:08 +0200

    Routes

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,4 @@
 class Section < ApplicationRecord
   has_many :section_edits
+  has_many :admin_users, :through => :section_edits
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root "demo#index"
+
+  # All REST controller routes
+  resource :subjects do
+    member do
+      get :delete
+    end
+  end
+
+
   get 'index/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
